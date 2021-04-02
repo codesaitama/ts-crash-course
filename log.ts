35a3252fbfb743947907cf58e0ba0f6f5aff2a19
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-02 22:56:41 +0100

    Updated the codes.

--- log.ts
+++ log.ts
@@ -2,4 +2,5 @@ export class Logger {
   static log(message: string) {
     console.log(message)
   }
+  
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 import { Logger } from './log';
+import { User } from './interfaces/IUser';
+import { UserAccount } from './classes/UserAccount'
 
-Logger.log('TypeScript works!')
+Logger.log('Data exchange!!!')
+
+const user: User = new UserAccount('Rafia Ana', 1);
+
+console.log(user);
