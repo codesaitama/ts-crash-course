74febc8620ceb633b9b94c18c4df343a06415e6d
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-03 19:32:16 +0100

    Modified the code.

--- main.ts
+++ main.ts
@@ -7,3 +7,17 @@ const user: User = new UserAccount('Rafia Ana', 1);
 Logger.log('Data exchange!!!')
 Logger.log(user);
 Logger.logType(true);
+
+const app = document?.querySelector('#root');
+const span = document?.createElement('span');
+const button = document?.createElement('button');
+
+span.textContent = 'Click Here';
+button.appendChild(span);
+button.className = 'btn--click';
+app?.appendChild(button);
+
+document?.querySelector('.btn--click').addEventListener('click', function(evnt){
+    console.log(evnt);
+});
+
