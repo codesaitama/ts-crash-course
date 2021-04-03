d0611165aa653e1b2f402a23a15889fb3db3befb
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-03 19:33:14 +0100

    Main.js file update.

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-const typeScriptVersion = '4.2.3';
+function compilerConfig(tsFile, importFile) {
 
-System.config({
-  transpiler: 'ts',
-  typescriptOptions: {
-  },
-  packages: {
-    ".": {
-      main: './main.ts',
-      defaultExtension: 'ts'
+  const typeScriptVersion = '4.2.3';
+
+  System.config({
+    transpiler: 'ts',
+    typescriptOptions: {
+    },
+    packages: {
+      ".": {
+        main: `./${tsFile}.ts`,
+        defaultExtension: 'ts'
+      }
+    },
+    meta: {
+      'typescript': { 'exports': 'ts' }
+    },
+    paths: {
+      'npm:': 'https://unpkg.com/'
+    },
+    map: {
+      'ts': 'npm:plugin-typescript@8.0.0/lib/plugin.js',
+      'typescript': 'npm:typescript@' + typeScriptVersion + '/lib/typescript.js'
     }
-  },
-  meta: {
-    'typescript': { 'exports': 'ts' }
-  },
-  paths: {
-    'npm:': 'https://unpkg.com/'
-  },
-  map: {
-    'ts': 'npm:plugin-typescript@8.0.0/lib/plugin.js',
-    'typescript': 'npm:typescript@' + typeScriptVersion + '/lib/typescript.js'
-  }
-});
+  });
 
-System.import('./main').then(function(e){console.log(e)})
-  .catch(console.error.bind(console));
+  System.import(`./${importFile}`)
+    .catch(console.error.bind(console));
+}
+
+compilerConfig('main', 'main')
