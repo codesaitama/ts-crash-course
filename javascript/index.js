9a999308a6735fdb72e0bef4d71d43c27c03bab2
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-29 10:20:32 +0100

    Working on some object manipulation code.

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,24 @@
-let person = {
+const person = {
     name: {
         firstname: 'Emmanuel',
         lastname: 'Achana'
+    },
+    method:{
+        getFullName(){
+            return `${this?.name?.firstname} ${this?.name?.lastname}`
+        }
     }
 }
 
-console.log(person?.name?.firstname)
+class Person{
+    constructor(){
+        
+    }
+}
+
+console.log(person?.name?.firstname);
+console.log(person?.method?.getFullName());
+
+const scores = new Map([['Ruby', 12], ['Steven', 11], ['Sam', 15], ['Robin', 14]]);
+
+console.log(scores);
